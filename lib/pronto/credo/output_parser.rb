f1b1620e5e7959833838b8c02de6a7e263df73b9
Tobiasz <tobiasz.malecki@appunite.com>
2017-07-22 20:51:14 +0200

    allow strict behaviour for --exit-code

--- lib/pronto/credo/output_parser.rb
+++ lib/pronto/credo/output_parser.rb
@@ -2,17 +2,30 @@ module Pronto
   module Credo
     class OutputParser
       attr_reader :output, :file
-      TYPE_WARNINGS = { 'R' => :info,
-                        'W' => :warning,
-                        'C' => :info,
-                        'D' => :info,
-                        'F' => :info,
-                        'A' => :info
-                      }
 
       def initialize(file, output)
         @file = file
         @output = output
+      end
+
+      def type_warnings
+        @type_warnings ||= if ENV["PRONTO_CREDO_STRICT"] == "1"
+          { 'R' => :warning,
+            'W' => :warning,
+            'C' => :warning,
+            'D' => :warning,
+            'F' => :warning,
+            'A' => :warning
+          }
+        else
+          { 'R' => :info,
+            'W' => :warning,
+            'C' => :info,
+            'D' => :info,
+            'F' => :info,
+            'A' => :info
+          }
+        end
       end
 
       def parse
@@ -22,10 +35,10 @@ module Pronto
           offence_in_line = line_parts[1]
           column_line = nil
           if line_parts[2].to_i == 0
-            offence_level = TYPE_WARNINGS[line_parts[2].strip]
+            offence_level = type_warnings[line_parts[2].strip]
             offence_message = line_parts[3..-1].join(':').strip
           else
-            offence_level = TYPE_WARNINGS[line_parts[3].strip]
+            offence_level = type_warnings[line_parts[3].strip]
             column_line = line_parts[2].to_i
             offence_message = line_parts[4..-1].join(':').strip
           end
